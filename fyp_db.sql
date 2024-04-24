a754f8c79bc0b0b24a025d90c88b96a240e8a23a
NAVEENNK\vvnnk <vvnnkk2600@gmail.com>
2024-04-25 00:22:16 +0530

    first commit

---
+++ fyp_db.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 24, 2024 at 05:47 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `fyp_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fyp_db`
+--
+
+CREATE TABLE `fyp_db` (
+  `id` int(11) NOT NULL,
+  `dept` varchar(20) NOT NULL,
+  `date_join` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fyp_db`
+--
+
